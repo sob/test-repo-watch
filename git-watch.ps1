f2164e6f2e459175e23184a968e45e25539a00cd
Sean O'Brien <sean@obrienfamily.org>
2021-03-30 02:05:24 -0500

    autosave

--- git-watch.ps1
+++ git-watch.ps1
@@ -61,7 +61,7 @@ try {
       $current_branch = git rev-parse --abbrev-ref HEAD
       git add -AN
       git commit -am $autosave_message --quiet 2>$null
-      git push origin
+      git push origin main
       git log --format="%C(auto)[$current_branch %h] %s" -n 1 --stat
 
       Write-Output ""
